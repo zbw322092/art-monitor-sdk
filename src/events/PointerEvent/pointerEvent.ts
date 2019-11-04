14269f0d2fa900d3258429895b7208525b519730
Bowen Zhong <zbw322092@hotmail.com>
2019-11-04 11:55:55 +0800

    pointer event

--- src/events/PointerEvent/pointerEvent.ts
+++ src/events/PointerEvent/pointerEvent.ts
@@ -1,5 +1,20 @@
 import { PointerEvent } from "../../enums/EventName";
+import { LoggerPointerEvent } from '../../logger/LoggerUIEvent/LoggerPointerEvent';
+import { TrackType } from "../../enums/TrackType";
+import { iDBStoreInstance } from "../../data-store/IDBStore";
+import { OBJECTNAME } from '../../constants/DB';
+import { throttle } from "../../utils/throttle";
 
-window.addEventListener(PointerEvent.pointermove, (event) => {
-  console.log('point event: ', event);
-});
+window.addEventListener(PointerEvent.pointermove, throttle((event) => {
+  
+  const loggerPointerEvent = new LoggerPointerEvent(TrackType.POINTEREVENT_POINTERMOVE, event);
+  console.log('Log Pointer: ', loggerPointerEvent);
+
+  iDBStoreInstance.set(OBJECTNAME, loggerPointerEvent)
+    .then(() => {
+      // console.log('click log added');
+    })
+    .catch((err) => {
+      console.log('click log err: ', err);
+    });
+}, 200));
--- src/logger/LoggerUIEvent/LoggerPointerEvent.ts
+++ src/logger/LoggerUIEvent/LoggerPointerEvent.ts
@@ -5,7 +5,23 @@ export class LoggerPointerEvent extends LoggerMouseEvent {
     super(TrackType, pointerEvent);
 
     this.pointerId = pointerEvent.pointerId
+    this.width = pointerEvent.width;
+    this.height = pointerEvent.height;
+    this.tangentialPressure = pointerEvent.tangentialPressure;
+    this.tiltX = pointerEvent.tiltX;
+    this.tiltY = pointerEvent.tiltY;
+    this.twist = pointerEvent.twist;
+    this.pointerType = pointerEvent.pointerType;
+    this.isPrimary = pointerEvent.isPrimary;
   }
 
   public pointerId: number;
+  public width: number;
+  public height: number
+  public tangentialPressure: number;
+  public tiltX: number;
+  public tiltY: number;
+  public twist: number;
+  public pointerType: string;
+  public isPrimary: boolean;
 }
